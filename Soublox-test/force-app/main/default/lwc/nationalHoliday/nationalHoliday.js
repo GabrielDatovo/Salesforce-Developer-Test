8ea3634871d4371f075e746bcf8a285ee8d26846
gabriel datovo <gabrielmontalbini@gmail.com>
2024-02-21 02:08:33 -0300

    new lwc and controller, adjust in class

---
+++ Soublox-test/force-app/main/default/lwc/nationalHoliday/nationalHoliday.js
@@ -0,0 +1,37 @@
+import { LightningElement, track, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getNationalHoliday from '@salesforce/apex/NationalHolidayController.getNationalHoliday';
+
+export default class NationalHoliday extends LightningElement {
+
+    @api recordId;
+    @track isHoliday;
+    @track name;
+    @track date;
+    @track remainingDays;
+    @track isLoading = true;
+
+    connectedCallback () {
+        getNationalHoliday()
+        .then( result => {
+            const resultParsed = JSON.parse(result);
+            this.isHoliday = resultParsed.isHoliday;
+            this.name = resultParsed.name;
+            if(!this.isHoliday){
+                this.date = resultParsed.date_x;
+                this.remainingDays = resultParsed.remainingDays;
+            }
+        })
+        .catch( error => {
+            const toastEvent = new ShowToastEvent({
+                title: 'Error',
+                message: error,
+                variant: 'error'
+            });
+            this.dispatchEvent(toastEvent);
+            console.log('error: ' + error);
+        })
+        .finally( () => {this.isLoading = false;});
+    }
+
+}
